bdc226f629a6cc67696c910e4c77f27bf50150f2
Martin Zackrisson <martin.zackrisson@fripost.org>
2020-10-03 12:45:40 +0200

    Walks

--- Assets/Scripts/PlayerWalking.cs
+++ Assets/Scripts/PlayerWalking.cs
@@ -6,7 +6,7 @@ public class PlayerWalking : MonoBehaviour
 {
     [SerializeField]
     AnimationCurve stepHeight;
-    [SerializeField, Range(0, 1)]
+    [SerializeField, Range(0, 1.5f)]
     float stepLength;
     [SerializeField, Range(0, 4)]
     float stepDuration;
@@ -36,6 +36,7 @@ public class PlayerWalking : MonoBehaviour
 
     IEnumerator<WaitForSeconds> Step()
     {
+        float factor = 0.02f;
         while (true) {
             if (Mathf.Abs(walkSpeed) > 0.1f) {
                 float duration = 0f;
@@ -43,14 +44,16 @@ public class PlayerWalking : MonoBehaviour
                 while (duration < stepDuration)
                 {
                     eyeCamera.transform.localPosition = cameraOrigin + transform.up * stepHeight.Evaluate(duration / stepDuration);
-                    yield return new WaitForSeconds(0.02f);
+                    yield return new WaitForSeconds(factor);
                     stepSpeed = Mathf.Max(stepSpeed, Mathf.Abs(walkSpeed));
-                    duration += stepSpeed * 0.02f;
-                    transform.position += transform.forward * walkSpeed * stepLength * 0.02f;
+                    duration += stepSpeed * factor;
+                    transform.position += transform.forward * walkSpeed * stepLength * factor;
+                    transform.Rotate(transform.up, turnSpeed * factor * 90f / turn90Duration);
                 }
                 eyeCamera.transform.localPosition = cameraOrigin + transform.up * stepHeight.Evaluate(1f);
             }
-            yield return new WaitForSeconds(0.02f);
+            transform.Rotate(transform.up, turnSpeed * factor * 90f / turn90Duration);
+            yield return new WaitForSeconds(factor);
         }
     }
 }
