414c9efab77123e4266323e465bcf6f9fd1dfb8c
Martin Zackrisson <martin.zackrisson@fripost.org>
2020-10-03 16:13:48 +0200

    monstret

---
+++ Assets/Scripts/Monster.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Monster : MonoBehaviour
+{
+    [SerializeField]
+    Transform player;
+
+    void Start()
+    {
+        NavMeshAgent agent = GetComponent<NavMeshAgent>();
+        agent.destination = player.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
