ad000cc1776b8e75e3f5fbbe26a737997c86e5d2
Martin Zackrisson <martin.zackrisson@fripost.org>
2020-10-03 13:34:24 +0200

    Working on look ui

--- Assets/Scripts/Eyes.cs
+++ Assets/Scripts/Eyes.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Eyes : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField, Range(1, 20)]
+    float detectionRange = 10f;
+
+    Camera cam;
+
     void Start()
     {
-        
+        cam = GetComponent<Camera>();    
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, 10f)) {
+            Interactable inter = hit.collider.gameObject.GetComponent<Interactable>();
+            if (inter != null)
+            {
+                inter.LookedAt(hit.distance);
+            }
+        }
     }
 }
---
+++ Assets/Scripts/UIInteractable.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIInteractable : MonoBehaviour
+{
+
+    static UIInteractable _instance;
+
+    public static void Place(Transform target)
+    {
+        Debug.Log(target);
+    }
+
+    public static bool canInteract {
+        set
+        {
+
+        }
+    }
+
+    public static void Hide()
+    {
+        Debug.Log("Hidden");
+    }
+
+    private void OnDestroy()
+    {
+        _instance = null;
+    }
+}
