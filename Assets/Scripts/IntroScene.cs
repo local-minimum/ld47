7e4fc8345658b2788c368e93407ad8a54f501989
Martin Zackrisson <martin.zackrisson@fripost.org>
2020-10-05 16:19:52 +0200

    Intro...ish

---
+++ Assets/Scripts/IntroScene.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class IntroScene : MonoBehaviour
+{
+    [SerializeField, Range(0, 10)]
+    float loadLevelAfter = 3f;
+
+    private void Update()
+    {
+        if (Time.timeSinceLevelLoad > loadLevelAfter)
+        {
+            SceneManager.LoadScene("scenes/Level");
+        }
+    }
+}
