b7dbd9a4ab32aed71e343fa6c2ad4711c4dd7194
Martin Zackrisson <martin.zackrisson@fripost.org>
2020-10-05 15:07:08 +0200

    Game end

---
+++ Assets/Scripts/GameEnd.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEnd : MonoBehaviour
+{
+
+    [SerializeField] GameObject[] activateOnEnter;
+
+    private void Start()
+    {
+        for (int i = 0; i < activateOnEnter.Length; i++)
+        {
+            activateOnEnter[i].SetActive(false);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            for (int i=0; i<activateOnEnter.Length; i++)
+            {
+                activateOnEnter[i].SetActive(true);
+            }
+        }
+    }
+}
