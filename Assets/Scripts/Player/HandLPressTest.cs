f6339e320f942b1006f8056751811bb59cea62a1
Kim Falck <flesh2plush@gmail.com>
2020-10-30 02:23:56 +0100

    Added cutlery models and test script for controlling left hand animation

---
+++ Assets/Scripts/Player/HandLPressTest.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandLPressTest : MonoBehaviour
+{
+    Animator animHandL;
+
+    void Start()
+    {
+        animHandL = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Q)) animHandL.SetTrigger("HandLPhoneTrigger");
+    }
+}
