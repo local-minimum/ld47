4f1c7fa1312498108259c145b2835f975ff927d9
Martin Zackrisson <martin.zackrisson@fripost.org>
2020-10-31 09:43:37 +0100

    simple respawn

---
+++ Assets/Scripts/RespawnFloor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RespawnFloor : MonoBehaviour
+{
+    [SerializeField] Transform respawnPoint;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            other.transform.SetParent(null);
+            other.transform.position = respawnPoint.position;
+            other.transform.rotation = respawnPoint.rotation;
+        }
+    }
+}
